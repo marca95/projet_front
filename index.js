d7b31147f366c01ac8bf16712776cfe21f0443ff
marca95 <pierre.majerus@outlook.be>
2024-05-03 11:11:27 +0200

    First ex for prof. title

--- index.js
+++ index.js
@@ -21,3 +21,16 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   startAnimation();
 });
+
+
+// Modal 
+
+function openModal() {
+  document.querySelector(".overlay").style.display = 'block';
+  document.querySelector(".modal").classList.add('modal-open');
+}
+
+function closeModal() {
+  document.querySelector(".overlay").style.display = 'none';
+  document.querySelector(".modal").classList.remove('modal-open');
+}
